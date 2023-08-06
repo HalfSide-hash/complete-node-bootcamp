70fe5ecb8ae56f558bda875a2384c8a563a856de
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2023-08-06 11:58:33 -0400

    changing the commits up, to the react async chain

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,15 +2,25 @@ const path = require("path");
 const fs = require('fs');
 
 //Synchronous way
-const textIn = fs.readFileSync(path.resolve(__dirname,'./final/txt/input.txt'), 'utf-8');
+// const textIn = fs.readFileSync(path.resolve(__dirname,'./final/txt/input.txt'), 'utf-8');
 
-const textOut = `Avacados are wild ${textIn}\nCreated On: ${Date.now()}`;
+// const textOut = `Avacados are wild ${textIn}\nCreated On: ${Date.now()}`;
 
-fs.writeFileSync(path.resolve(__dirname,'./final/txt/output.txt'), textOut);
+// fs.writeFileSync(path.resolve(__dirname,'./final/txt/output.txt'), textOut);
 
-console.log('file written');
+// console.log('file written');
 
 //Async way
-const textInAsync = fs.readFile(path.resolve(__dirname,'./final/txt/start.txt'), 'utf-8', (err,data) => {
-    console.log(data);
+fs.readFile(path.resolve(__dirname,'./final/txt/start.txt'), 'utf-8', (err,data) => {
+    fs.readFile(path.resolve(__dirname,`./final/txt/${data}.txt`), 'utf-8', (err,data2) => {
+        console.log(data2);
+        fs.readFile(path.resolve(__dirname,`./final/txt/append.txt`), 'utf-8', (err,data3) => {
+            console.log(data3);
+            fs.writeFile(path.resolve(__dirname,`./final/txt/final.txt`), `${data2}\n${data3}\nWritten On: ${Date.now()}`, 'utf-8', err => {
+                console.log('written boi');
+            });
+        });
+    });
 });
+
+console.log('reading first');
