981e24e08abb618f09b7902506b11ef64e00641d
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2023-08-05 23:14:08 -0400

    showing the sync and async examples

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,14 +1,15 @@
 const path = require("path");
 const fs = require('fs');
 
-fs.readFile(path.resolve(__dirname,'./final/txt/input.txt'), 'utf-8', (err,data) => {
+/*const textIn = fs.readFile(path.resolve(__dirname,'./final/txt/input.txt'), 'utf-8', (err,data) => {
     console.log(data);
-});
+});*/
 
-console.log('Read that file dawg');
+const textIn = fs.readFileSync(path.resolve(__dirname,'./final/txt/input.txt'), 'utf-8');
+//console.log('Read that file dawg');
 
-//const textOut = `Avacados are wild ${textIn}\nCreated On: ${Date.now()}`;
+const textOut = `Avacados are wild ${textIn}\nCreated On: ${Date.now()}`;
 
-//fs.writeFileSync(path.resolve(__dirname,'./final/txt/output.txt'), textOut);
+fs.writeFileSync(path.resolve(__dirname,'./final/txt/output.txt'), textOut);
 
-//console.log('file written')
+console.log('file written')
