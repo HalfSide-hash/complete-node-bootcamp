017603f4f56e7c599090458f29b805c3b85340ae
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2023-08-14 15:13:49 -0400

    api quick lesson

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //FILES
 //Synchronous way
@@ -30,11 +31,37 @@ console.log('reading first'); */
 
 ///////////////////////////
 // SERVER
+
+const data = fs.readFileSync(`${__dirname}/final/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) =>{
-    console.log(req);
-    res.end('The server talkin');
+    console.log(req.url);
+    const pathName = req.url;
+    if (pathName === '/' || pathName === '/overview'){
+        res.end('We OVERVIEWING BOIS');
+    }
+    else if (pathName === '/product'){
+            res.end('We PRODUCTING BOIS');
+    }
+    else if(pathName === '/api'){
+        res.writeHead(200, {
+            'Content-type' : 'application/json'
+        });
+        res.end(data);
+
+    } 
+    else{
+        res.writeHead(404, {
+            'Content-type' : 'text/html',
+            'my-own-header' : 'hello-world'
+        });
+        res.end('<h1>Page not found</h1>');
+    }
+    //res.end('The server talkin');
 });
 
 server.listen(8000, '127.0.0.1', () => {
     console.log('Listening to requests on port 8000');
-})
+});
+
