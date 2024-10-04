939496436a5b1718e8b8ee4b2907c28d8b613b39
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-03 20:37:23 -0400

    i like 120

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -34,36 +34,33 @@ console.log('reading first'); */
 ///////////////////////////
 // SERVER
 
-const tempOverview = fs.readFileSync(
-  `${__dirname}/templates/template-overview.html`,
-  'utf-8'
-);
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  'utf-8'
-);
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  'utf-8'
-);
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+const slugs = dataObj.map((el) =>
+  slugify(el.productName, {
+    lower: true,
+  })
+);
 
 console.log(slugs);
-console.log(slugify('Fresh Avocados', { lower: true }));
+console.log(
+  slugify('Fresh Avocados', {
+    lower: true,
+  })
+);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
   if (pathname === '/' || pathname === '/overview') {
-    const cardHtml = dataObj
-      .map((el) => replaceTemplate(tempCard, el))
-      .join('');
+    const cardHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join('');
     const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardHtml);
     res.writeHead(200, {
       'Content-type': 'text/html',
