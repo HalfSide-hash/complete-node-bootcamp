fe7c53ed0d1a1cd624d7d33d6fe4bb6c02670d38
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-03 20:20:02 -0400

    changes

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 const replaceTemplate = require('./modules/replaceTemplate');
+const slugify = require('slugify');
 
 //FILES
 //Synchronous way
@@ -38,6 +39,11 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, {lower:true}));
+
+console.log(slugs);
+console.log(slugify('Fresh Avocados', {lower : true}));
 
 const server = http.createServer((req, res) =>{
 
