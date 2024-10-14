ab69e3a8b3a1419c238b26f74788cab2b21c5f84
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-14 12:30:18 -0400

    export 2

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -2,7 +2,8 @@
 //console.log(require('module').wrapper);
 
 const C = require('./test-module-1');
-
 const calc1 = new C();
-
 console.log(calc1.add(2, 5));
+
+const calc2 = require('./test-module-2');
+console.log(calc2.multiply(2, 5));
