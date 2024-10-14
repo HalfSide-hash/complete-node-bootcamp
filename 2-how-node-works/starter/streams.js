f56a4c3958493111ddee375f51f4a33372ea17a4
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-14 12:12:45 -0400

    stream code - solution 3 is the usual way

--- 2-how-node-works/starter/streams.js
+++ 2-how-node-works/starter/streams.js
@@ -8,7 +8,7 @@ server.on('request', (req, res) => {
     res.end(data);
   });*/
 
-  //Solution 2: Streams
+  /* //Solution 2: Streams
   const readable = fs.createReadStream('testtt-file.txt');
   readable.on('data', (chunk) => {
     res.write(chunk);
@@ -20,7 +20,12 @@ server.on('request', (req, res) => {
     console.log(err);
     res.statusCode = 500;
     res.end('File not found');
-  });
+  });*/
+
+  // Solution 3:
+  const readable = fs.createReadStream('test-file.txt');
+  readable.pipe(res);
+  // readableSource.pipe(writableDest);
 });
 
 server.listen(8000, '127.0.0.1', () => {
