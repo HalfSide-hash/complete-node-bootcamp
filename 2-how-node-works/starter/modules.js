c206c3f6cdf0d182efd67a6a95621f8189305645
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-14 12:31:30 -0400

    deconstructing module 2

--- 2-how-node-works/starter/modules.js
+++ 2-how-node-works/starter/modules.js
@@ -5,5 +5,5 @@ const C = require('./test-module-1');
 const calc1 = new C();
 console.log(calc1.add(2, 5));
 
-const calc2 = require('./test-module-2');
-console.log(calc2.multiply(2, 5));
+const { add, multiply, divide } = require('./test-module-2');
+console.log(multiply(2, 5));
