b531c6a9a688fab189e0c2e67b1565c8a12a8ce6
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-14 11:36:36 -0400

    event driven architecture

--- 2-how-node-works/starter/events.js
+++ 2-how-node-works/starter/events.js
@@ -1,4 +1,5 @@
 const EventEmitter = require('events');
+const http = require('http');
 
 class Sales extends EventEmitter {
   constructor() {
@@ -20,3 +21,25 @@ myEmitter.on('newSale', (stock) => {
   console.log(`There are now ${stock} items left in stock.`);
 });
 myEmitter.emit('newSale', 9);
+
+/////////////////////////////
+
+const server = http.createServer();
+
+server.on('request', (req, res) => {
+  console.log('Request received');
+  console.log(req.url);
+  res.end('Reuqest received');
+});
+
+server.on('request', (req, res) => {
+  console.log('RAnotha request');
+});
+
+server.on('close', () => {
+  console.log('Server closed');
+});
+
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Waiting for requests...');
+});
