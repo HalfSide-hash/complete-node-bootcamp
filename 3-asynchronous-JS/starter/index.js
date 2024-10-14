785c3033e19757feb2cf994e735e9e24837a5ea0
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-14 16:04:02 -0400

    new change

--- 3-asynchronous-JS/starter/index.js
+++ 3-asynchronous-JS/starter/index.js
@@ -10,17 +10,24 @@ const readFilePro = (file) => {
   });
 };
 
+const writeFilePro = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(file, data, (err) => {
+      if (err) reject('We failed te files bois');
+      resolve('success');
+    });
+  });
+};
+
 readFilePro(`${__dirname}/dog.txt`).then((data) => {
   console.log(`Breed: ${data}`);
-
-  superagent
+  return superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .then((res) => {
-      console.log(res.body.message);
-
-      fs.writeFile('dog-img.txt', res.body.message, (err) => {
-        console.log('Random dog image saved to file');
-      });
+      return writeFilePro('dog-img.txt', res.body.message);
+    })
+    .then(() => {
+      console.log('Random dog image saved to file');
     })
     .catch((err) => {
       console.log(err.message);
