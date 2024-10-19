58873261cbb9714e0a672aa0a3e3fd6c5ed4b4cc
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-18 21:06:29 -0400

    patch example

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -46,6 +46,23 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+  if (Number(req.params.id) > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: '',
+    },
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
