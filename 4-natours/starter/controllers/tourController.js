42161221abf302e0effb718c5ceeaea8528d0734
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-10-22 23:54:46 -0400

    good example of delete, forgot the 204

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -76,9 +76,9 @@ exports.updateTour = async (req, res) => {
 exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id);
-    res.status(200).json({
+    res.status(204).json({
       status: 'success',
-      message: 'Entity deleted',
+      data: null,
     });
   } catch (err) {
     res.status(404).json({
