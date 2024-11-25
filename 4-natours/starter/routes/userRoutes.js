487f8d0a0c64b2942e5369c778d9334a65c37e10
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-11-24 21:51:27 -0500

    putting auth protection throughout user routes

--- 4-natours/starter/routes/userRoutes.js
+++ 4-natours/starter/routes/userRoutes.js
@@ -4,25 +4,20 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
+//No AUTH needed
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
-
-router.patch(
-  '/updateMyPassword',
-  authController.protect,
-  authController.updatePassword,
-);
-
-router.get(
-  '/me',
-  authController.protect,
-  userController.getMe,
-  userController.getUser,
-);
-router.patch('/updateMe', authController.protect, userController.updateMe);
-router.delete('/deleteMe', authController.protect, userController.deleteMe);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+//From here on out, we need it
+router.use(authController.protect);
+
+router.patch('/updateMyPassword', authController.updatePassword);
+
+router.get('/me', userController.getMe, userController.getUser);
+router.patch('/updateMe', userController.updateMe);
+router.delete('/deleteMe', userController.deleteMe);
 
 router
   .route('/')
