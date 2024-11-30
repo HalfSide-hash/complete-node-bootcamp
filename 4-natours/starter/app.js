5541ce82a25a60a1845e6ef58a6458ac605fb39b
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-11-30 15:30:08 -0500

    setting up pug

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -13,6 +14,11 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '/views'));
+
+//Serving static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 // MIDDLEWARES
 
@@ -55,9 +61,6 @@ app.use(
   }),
 );
 
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 //Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -65,6 +68,9 @@ app.use((req, res, next) => {
 });
 
 // ROUTES
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
