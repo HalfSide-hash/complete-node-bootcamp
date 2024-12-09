70ac25dd895aefbb907634d4b90ea5a03d019938
NaturalSlecti0n <sylvester.delvillar@gmail.com>
2024-12-08 23:11:36 -0500

    the map work

---
+++ 4-natours/starter/public/js/mapbox.js
@@ -0,0 +1,43 @@
+/* eslint-disable */
+const locations = JSON.parse(document.getElementById('map').dataset.locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1Ijoic3lsY2hpbGwiLCJhIjoiY200Z2d6eHF4MGFjZzJqcHA4NnhqY3o4OSJ9.mzVfTFpoc-OF2o3DZf5xvQ';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v11',
+  scrollZoom: false,
+  //   center: [-118.113491, 34.111745],
+  //   zoom: 10,
+  //   interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((loc) => {
+  const el = document.createElement('div');
+  el.className = 'marker';
+
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates, {
+      padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100,
+      },
+    })
+    .addTo(map);
+
+  new mapboxgl.Popup()
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Day ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds);
