4aa5856ef3f27d11a7c8734abc0f654c45bb4526
HalfSide-hash <sylvester.delvillar@gmail.com>
2024-12-28 17:04:37 -0500

    without api approach

--- 4-natours/starter/controllers/viewsController.js
+++ 4-natours/starter/controllers/viewsController.js
@@ -54,5 +54,20 @@ exports.getLoginForm = catchAsync(async (req, res) => {
 });
 
 exports.updateUserData = catchAsync(async (req, res) => {
-  const user = await User.findByIdAndUpdate(req.user.id);
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
+
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
 });
