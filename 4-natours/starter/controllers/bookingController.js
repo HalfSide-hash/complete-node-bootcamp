ef415553089a588fcc66a39f856c31a496356d30
HalfSide-hash <sylvester.delvillar@gmail.com>
2025-01-18 20:31:09 -0500

    stripe backend integratiom

---
+++ 4-natours/starter/controllers/bookingController.js
@@ -0,0 +1,40 @@
+const Tour = require('../models/tourModel');
+const APIFeatures = require('../utils/apiFeatures');
+const multer = require('multer');
+const sharp = require('sharp');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+const factory = require('./handlerFactory');
+
+exports.getCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourID);
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourID,
+    mode: 'payment',
+    line_items: [
+      {
+        quantity: 1,
+        price_data: {
+          currency: 'usd',
+          unit_amount: tour.price * 100,
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary, //description here
+            images: [`https://natours.dev/img/tours/${tour.imageCover}`],
+          },
+        },
+      },
+    ],
+  });
+
+  res.status(200).json({
+    status: 'success',
+    session,
+  });
+});
